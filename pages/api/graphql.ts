04ca30bf5a424d7178db0ba9d9220f621b2aee75
William Ravel <wravel@gmail.com>
2021-01-31 21:53:02 +0100

    Add GraphQL code generator for the back end

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, gql } from "apollo-server-micro";
+import { ApolloServer, gql, UserInputError } from "apollo-server-micro";
 import mysql from "serverless-mysql";
 import { OkPacket } from "mysql";
 import { Resolvers, TaskStatus } from "../../generated/graphql-backend";
@@ -47,7 +47,24 @@ interface TaskDbRow {
   task_status: TaskStatus;
 }
 
+type TasksDbQueryResult = TaskDbRow[];
+
 type TaskDbQueryResult = TaskDbRow[];
+
+const getTaskById = async (id: number, db: mysql.ServerlessMysql) => {
+  const tasks = await db.query<TaskDbQueryResult>(
+    "SELECT id, title, task_status FROM tasks WHERE id= ?",
+    [id]
+  );
+
+  return tasks.length
+    ? {
+        id: tasks[0].id,
+        title: tasks[0].title,
+        status: tasks[0].task_status,
+      }
+    : null;
+};
 
 const resolvers: Resolvers<ApolloContext> = {
   Query: {
@@ -59,7 +76,7 @@ const resolvers: Resolvers<ApolloContext> = {
         query += " WHERE task_status = ?";
         queryParams.push(status);
       }
-      const tasks = await context.db.query<TaskDbQueryResult>(
+      const tasks = await context.db.query<TasksDbQueryResult>(
         query,
         queryParams
       );
@@ -70,8 +87,8 @@ const resolvers: Resolvers<ApolloContext> = {
         status: task_status,
       }));
     },
-    task(parent, args, context) {
-      return null;
+    async task(parent, args, context) {
+      return getTaskById(args.id, context.db);
     },
   },
   Mutation: {
@@ -86,11 +103,37 @@ const resolvers: Resolvers<ApolloContext> = {
         status: TaskStatus.Active,
       };
     },
-    updateTask(parent, args, context) {
-      return null;
+    async updateTask(parent, args, context) {
+      const columns: string[] = [];
+      const sqlParams: any[] = [];
+
+      if (args.input.title) {
+        columns.push("title = ?");
+        sqlParams.push(args.input.title);
+      }
+      if (args.input.status) {
+        columns.push("task_status = ?");
+        sqlParams.push(args.input.status);
+      }
+      sqlParams.push(args.input.id);
+
+      await context.db.query(
+        `UPDATE tasks SET ${columns.join(",")} WHERE id = ?`,
+        sqlParams
+      );
+
+      const updatedTask = await getTaskById(args.input.id, context.db);
+
+      return updatedTask;
     },
-    deleteTask(parent, args, context) {
-      return null;
+    async deleteTask(parent, args, context) {
+      const task = await getTaskById(args.id, context.db);
+      if (!task) {
+        throw new UserInputError("Could not find your task.");
+      } else {
+        await context.db.query("DELETE FROM tasks WHERE id = ?", [args.id]);
+      }
+      return task;
     },
   },
 };
