3d31d8ee40df9c2140c658b9f9612019adab80b1
wravel <wravel@gmail.com>
2021-01-31 15:44:11 +0100

    Add GraphQL API route

--- pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from "next"
-import { NextApiRequestQuery } from "next/dist/next-server/server/api-utils"
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
